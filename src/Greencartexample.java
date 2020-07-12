6ba1681d7d045a522a8d4ded47d9e9e11a9533c3
Reshmi Raman <reshmi.raman@gmail.com>
2020-07-12 10:08:09 -0400

    updated gitignore

--- src/Greencartexample.java
+++ src/Greencartexample.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/actions/Linkscount.java
+++ src/actions/Linkscount.java
@@ -1,14 +1,13 @@
 package actions;
 
+import java.util.Iterator;
 import java.util.Set;
 
-import java.util.Iterator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.*;
 
 public class Linkscount {
 
